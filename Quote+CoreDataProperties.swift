745854bccdccdf82e85ca96361f566457445b94d
Jared Breedlove <Jaredtechb@gmail.com>
2019-04-13 13:46:04 -0400

    Added images and quotes to coredata

---
+++ Quote+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Quote+CoreDataProperties.swift
+//  Dash-New
+//
+//  Created by Jared Breedlove on 4/13/19.
+//  Copyright © 2019 iMac. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Quote {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Quote> {
+        return NSFetchRequest<Quote>(entityName: "Quote")
+    }
+
+    @NSManaged public var text: String?
+    @NSManaged public var xpos: Float
+    @NSManaged public var ypos: Float
+    @NSManaged public var scale: Float
+    @NSManaged public var layer: Int16
+    @NSManaged public var board: Board?
+
+}
