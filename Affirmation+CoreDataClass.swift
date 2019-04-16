5f010a37126c9f2a33c5932e9a8dc58ec3a8334f
Jared Breedlove <Jaredtechb@gmail.com>
2019-04-16 15:38:08 -0400

    Refactor Affirmations to correct naming

---
+++ Affirmation+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Affirmation+CoreDataClass.swift
+//  Dash-New
+//
+//  Created by Jared Breedlove on 4/16/19.
+//  Copyright © 2019 iMac. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Affirmation)
+public class Affirmation: NSManagedObject {
+
+}
